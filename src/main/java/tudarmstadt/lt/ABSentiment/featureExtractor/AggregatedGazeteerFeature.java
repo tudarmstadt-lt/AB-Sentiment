bf415f82c851679b135c9fcfef42380f02ed6068
Abhishek <abhirocks089@gmail.com>
2017-07-24 16:15:34 +0200

    Added new feature

---
+++ src/main/java/tudarmstadt/lt/ABSentiment/featureExtractor/AggregatedGazeteerFeature.java
@@ -0,0 +1,153 @@
+package tudarmstadt.lt.ABSentiment.featureExtractor;
+
+import de.bwaldvogel.liblinear.Feature;
+import de.bwaldvogel.liblinear.FeatureNode;
+import org.apache.uima.jcas.JCas;
+import tudarmstadt.lt.ABSentiment.uimahelper.Preprocessor;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Created by abhishek on 20/6/17.
+ */
+public class AggregatedGazeteerFeature implements FeatureExtractor {
+
+    private int offset = 0;
+
+    private HashMap<String, HashSet<Integer>> termIndexMap = new HashMap<>();
+    private HashMap<String, Integer> labelIndexMap = new HashMap<>();
+    private ArrayList<String> terms = new ArrayList<>();
+    private int totalNumberOfLabels;
+    private int featureCount;
+    private int columnType;
+
+    private Preprocessor preprocessor = new Preprocessor(true);
+
+    /**
+     * Constructor; specifies the dense gazetteer file. Feature offset is set to '0' by default.
+     * @param denseGazetteer path to a file containing word with/without their polarity
+     */
+    public AggregatedGazeteerFeature(String denseGazetteer) {
+        loadGazeteerFile(denseGazetteer);
+    }
+
+    /**
+     * Constructor; specifies the dense gazetteer file. Feature offset is specified.
+     * @param denseGazetteer path to a file containing word with/without their polarity
+     * @param offset the feature offset, all features start from this offset
+     */
+    public AggregatedGazeteerFeature(String denseGazetteer, int offset) {
+        this(denseGazetteer);
+        this.offset = offset;
+    }
+
+    @Override
+    public Feature[] extractFeature(JCas cas) {
+        Collection<String> documentText = preprocessor.getTokenStrings(cas);
+        double[] featureVector = new double[featureCount];
+        float documentLength = documentText.size();
+
+        for(int i = 0;i<featureVector.length;i++){
+            featureVector[i] = 0;
+        }
+
+        if(columnType == 1){
+            for(String token:documentText){
+                if(terms.contains(token)){
+                    featureVector[0]++;
+                }
+            }
+        }else if(columnType == 2){
+            for(String token:documentText){
+                if(termIndexMap.containsKey(token)){
+                    for(Integer index:termIndexMap.get(token)){
+                        featureVector[index]++;
+                    }
+                }
+            }
+        }
+        Feature[] features = new Feature[featureCount];
+
+        if(documentLength != 0) {
+            for(int i = 0;i<featureVector.length;i++){
+                featureVector[i] /= documentLength;
+            }
+        }
+        for(int i=0;i<featureCount;i++){
+            features[i] = new FeatureNode(i + offset + 1, featureVector[i]);
+        }
+        return features;
+    }
+
+    @Override
+    public int getFeatureCount() {
+        return featureCount;
+    }
+
+    @Override
+    public int getOffset() {
+        return offset;
+    }
+
+
+    /**
+     * Loads a word list with words with/without their polarity.
+     * @param fileName path to a file containing word with/without their polarity
+     */
+    private void loadGazeteerFile(String fileName) {
+        BufferedReader br;
+        String line;
+        int i = 0;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
+            if ((line = br.readLine()) != null) {
+                String[] tokenLine = line.split("\\t");
+                if (tokenLine.length == 0) {
+                    columnType = 0;
+                }else if(tokenLine.length == 1 ){
+                    columnType = 1;
+                }else{
+                    columnType = 2;
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
+            if(columnType == 1){
+                while ((line = br.readLine()) != null) {
+                    String[] tokenLine = line.split("\\t");
+                    terms.add(tokenLine[0]);
+                }
+                i++;
+            }else if(columnType == 2){
+                while ((line = br.readLine()) != null) {
+                    String[] tokenLine = line.split("\\t");
+                    if(!labelIndexMap.containsKey(tokenLine[tokenLine.length-1])){
+                        labelIndexMap.put(tokenLine[tokenLine.length-1], i++);
+                    }
+                    HashSet<Integer> indexHashSet;
+                    if(!termIndexMap.containsKey(tokenLine[0])){
+                        indexHashSet  = new HashSet<>();
+                    }else{
+                        indexHashSet = termIndexMap.get(tokenLine[0]);
+                    }
+                    indexHashSet.add(labelIndexMap.get(tokenLine[tokenLine.length-1]));
+                    termIndexMap.put(tokenLine[0], indexHashSet);
+                }
+            }
+            totalNumberOfLabels = i;
+            featureCount = totalNumberOfLabels;
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/tudarmstadt/lt/ABSentiment/featureExtractor/PolarityLexiconFeature.java
@@ -0,0 +1,121 @@
+package tudarmstadt.lt.ABSentiment.featureExtractor;
+
+import de.bwaldvogel.liblinear.Feature;
+import de.bwaldvogel.liblinear.FeatureNode;
+import org.apache.uima.jcas.JCas;
+import tudarmstadt.lt.ABSentiment.uimahelper.Preprocessor;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * Created by abhishek on 11/7/17.
+ */
+public class PolarityLexiconFeature implements FeatureExtractor{
+
+    private int offset = 0;
+
+    private HashMap<String, Float[]> termPolarityMap = new HashMap<>();
+    private int totalNumberOfLabels;
+    private int featureCount;
+    private int numberOfTokensFound;
+
+    private Preprocessor preprocessor = new Preprocessor(true);
+
+    /**
+     * Constructor; specifies the dense gazetteer file. Feature offset is set to '0' by default.
+     * @param termPolarityFile path to a file containing word with their polarity vector
+     */
+    public PolarityLexiconFeature(String termPolarityFile) {
+        loadPolarityFile(termPolarityFile);
+    }
+
+    /**
+     * Constructor; specifies the dense gazetteer file. Feature offset is specified.
+     * @param termPolarityFile path to a file containing word with their polarity vector
+     */
+    public PolarityLexiconFeature(String termPolarityFile, int offset) {
+        this(termPolarityFile);
+        this.offset = offset;
+    }
+
+    @Override
+    public Feature[] extractFeature(JCas cas) {
+        Collection<String> documentText = preprocessor.getTokenStrings(cas);
+        float featureVector[] = new float[featureCount];
+        Feature[] features = new Feature[featureCount];
+        for(int i = 0;i<featureVector.length;i++){
+            featureVector[i] = 0.0f;
+        }
+        numberOfTokensFound = 0;
+        for(String token:documentText){
+            if(termPolarityMap.containsKey(token)){
+                Float polarity[] = termPolarityMap.get(token);
+                for(int i=0;i<totalNumberOfLabels;i++){
+                    featureVector[i]+=polarity[i];
+                }
+            }
+            numberOfTokensFound++;
+        }
+        if(numberOfTokensFound != 0) {
+            for(int i = 0;i<featureVector.length;i++){
+                featureVector[i] /= numberOfTokensFound;
+            }
+        }
+        for(int i=0;i<featureCount;i++){
+            features[i] = new FeatureNode(i + offset + 1, featureVector[i]);
+        }
+        return features;
+    }
+
+    @Override
+    public int getFeatureCount() {
+        return featureCount;
+    }
+
+    @Override
+    public int getOffset() {
+        return offset;
+    }
+
+
+    /**
+     * Loads a word list with words with/without their polarity.
+     * @param fileName path to a file containing word with their polarity vector
+     */
+    public void loadPolarityFile(String fileName) {
+        try {
+            BufferedReader br;
+            if (fileName.endsWith(".gz")) {
+                br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fileName)), "UTF-8"));
+            } else {
+                br = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
+            }
+            String line;
+            int size=0;
+            if((line = br.readLine()) != null){
+                size = line.split("\t").length-1;
+            }
+            Float polarity[] = new Float[size];
+            while(line != null){
+                String term[] = line.split("\t");
+                for(int i=0;i<size;i++){
+                    polarity[i] = Float.parseFloat(term[i+1]);
+                }
+                termPolarityMap.put(term[0], polarity);
+                line = br.readLine();
+            }
+            totalNumberOfLabels = size;
+            featureCount = totalNumberOfLabels;
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
